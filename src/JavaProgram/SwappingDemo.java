393deb060c5fc7c057c23472060bae46ad510910
Dnayneshwar1Ghate <dnyaneshwarghate1010@gmail.com>
2024-11-20 01:02:04 +0530

    3 way for swapping

--- src/JavaProgram/SwappingDemo.java
+++ src/JavaProgram/SwappingDemo.java
@@ -3,10 +3,32 @@ package JavaProgram;
 public class SwappingDemo {
 	
 	public static void main(String[] args) {
-		FistWay();
+		System.out.println("_________________________");
+		fistWay();
+		System.out.println("_________________________");
+		secondWay();
+		System.out.println("_________________________");
+		thirdWay();
+		System.out.println("_________________________");
 	}
 	
-	public static void FistWay()
+	private static void thirdWay() {
+		
+		int a=3;
+		int b=5;
+		System.out.println("Before Swap");
+		System.out.println(a+"   "+b);
+		
+		a=a*b;    // 3*5=15
+		b=a/b;    //15/5= 3  -->B
+		a=a/b;    //15/3=  5   --->>A
+		
+		System.out.println("After Swap");
+		System.out.println(a+"   "+b);
+		
+	}
+
+	private static void fistWay()
 	{
 		int a=10; int b=20;
 		System.out.println("Before Swap");
@@ -15,6 +37,22 @@ public class SwappingDemo {
 		temp=a;
 		a=b;
 		b=temp;
+		System.out.println("After Swap");
+		System.out.println(a+"   "+b);
+		
+	}
+	private static void secondWay()
+	{
+		int a=100;
+		int b=200;
+		System.out.println("Before Swap");
+		System.out.println(a+"   "+b);
+		
+		a=a+b; //100+200=300
+		b=a-b;  //300-200=100  B=100
+		a=a-b;  //300-100=200  A=200
+		
+		
 		System.out.println("After Swap");
 		System.out.println(a+"   "+b);
 		
